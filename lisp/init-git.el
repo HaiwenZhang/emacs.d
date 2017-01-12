53e9dc727f7f5534b64912ee500a091f9842ac30
Haiwen <haiwensummer@gmail.com>
2017-01-12 20:42:09 +0800

    Change magit config and org config

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,14 +1,48 @@
 ;; git gutter config
 (require-package 'git-gutter-fringe)
 (require 'git-gutter-fringe)
-(set-face-foreground 'git-gutter-fr:modified "yellow")
-(set-face-foreground 'git-gutter-fr:added    "blue")
-(set-face-foreground 'git-gutter-fr:deleted  "red")
+(set-face-foreground 'git-gutter-fr:modified "#FF00FF")
+(set-face-foreground 'git-gutter-fr:added    "#00FF00")
+(set-face-foreground 'git-gutter-fr:deleted  "#FF0000")
+;; Please adjust fringe width if your own sign is too big.
+(setq-default left-fringe-width  20)
+(setq-default right-fringe-width 20)
+
+(fringe-helper-define 'git-gutter-fr:added nil
+  ".XXXXXX."
+  "XX....XX"
+  "X......X"
+  "X......X"
+  "XXXXXXXX"
+  "XXXXXXXX"
+  "X......X"
+  "X......X")
+
+(fringe-helper-define 'git-gutter-fr:deleted nil
+  "XXXXXX.."
+  "XX....X."
+  "XX.....X"
+  "XX.....X"
+  "XX.....X"
+  "XX.....X"
+  "XX....X."
+  "XXXXXX..")
+
+(fringe-helper-define 'git-gutter-fr:modified nil
+  "XXXXXXXX"
+  "X..XX..X"
+  "X..XX..X"
+  "X..XX..X"
+  "X..XX..X"
+  "X..XX..X"
+  "X..XX..X"
+  "X..XX..X")
 (global-git-gutter-mode +1)
 
 ;; magit config
 (require-package 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+(global-set-key (kbd "C-x M-l") 'magit-log-buffer-file)
 
 (provide 'init-git)
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,9 @@
 (require-package 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
 
 
 (provide 'init-org)
