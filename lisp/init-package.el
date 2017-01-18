1bbf7a4557029aad36884e294f854e9161c3de9c
Haiwen <haiwensummer@gmail.com>
2017-01-18 13:07:48 +0800

    Add package manage config

---
+++ lisp/init-package.el
@@ -0,0 +1,15 @@
+(setq inhibit-startup-message t)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(provide 'init-package)
