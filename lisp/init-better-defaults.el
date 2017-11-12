f7c4a47fbf707b8fa2469903f03a5560b46c0f98
HaiwenZhang <haiwensummer@gmail.com>
2017-11-12 16:03:28 +0800

    Add windows move function

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -21,6 +21,26 @@
 (use-package all-the-icons
   :ensure t)
 
+(use-package windresize
+  :ensure t
+  :config (global-set-key (kbd "C-c m") 'windresize))
+
+;;;Winner-mode
+;;可以使用 Ctrl-c ← （就是向左的箭头键）组合键，退回你的上一个窗口设置。）
+;;可以使用 Ctrl-c → （前进一个窗口设置。）
+(when (fboundp 'winner-mode) 
+  (winner-mode) 
+  (windmove-default-keybindings))
+
+
+;;;windmove-mode
+(when (fboundp 'windmove-default-keybindings)
+      (windmove-default-keybindings)
+    (global-set-key (kbd "C-c j")  'windmove-left)
+    (global-set-key (kbd "C-c l") 'windmove-right)
+    (global-set-key (kbd "C-c i")    'windmove-up)
+    (global-set-key (kbd "C-c k")  'windmove-down))
+
 (use-package neotree
   :ensure t
   :bind ("<f8>" . neotree-toggle)
